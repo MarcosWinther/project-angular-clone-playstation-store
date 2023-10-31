8875029a5c9d68601095877feeedb82734c1c1fa
MarcosWinther <winthermarcos@gmail.com>
2023-10-30 23:18:14 -0300

    Criado a base dos components e do CSS Reset

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,11 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+		<app-menu-bar></app-menu-bar>
+		<app-home></app-home>
+		<router-outlet></router-outlet>
+	`
 })
 export class AppComponent {
   title = 'store';
